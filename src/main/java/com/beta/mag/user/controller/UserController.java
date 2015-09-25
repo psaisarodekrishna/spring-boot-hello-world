b077b1d4d0c079e3f6ba8db9e312aa860fe321c4
psaisarodekrishna <psaisarodekrishna@deloitte.com>
2015-09-25 15:08:29 +0530

    Final commit

--- src/main/java/com/beta/mag/user/controller/UserController.java
+++ src/main/java/com/beta/mag/user/controller/UserController.java
@@ -13,10 +13,11 @@
 
 package com.beta.mag.user.controller;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,64 +27,72 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.heroku.sdk.jdbc.DatabaseUrl;
 
-
 ;
 
 /**
- * The Class UserPersistence. controller is to create new user. returns the json
- * response to the REST client.
+ * The Class UserPersistence. controller is to create new user. returns the json response to the
+ * REST client.
  */
 @RestController
 @RequestMapping(value = "/api")
 public class UserController {
 
+    public Statement openConnection() {
+        Connection connection = null;
+        try {
+            connection = DatabaseUrl.extract().getConnection();
+            Statement stmt = connection.createStatement();
+            return stmt;
+        } catch (Exception e) {
+            System.out.println("Exception during connection opening");
+        } finally {
+            if (connection != null)
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                }
+        }
+        return null;
+    }
 
-
-	@RequestMapping(value = "/v10/user/{userId}/{passCode}", method = RequestMethod.GET)
-	@ResponseBody
-	public String getUserProfile(@PathVariable final String userId,
-			@PathVariable final String passCode)  {
-		System.out.println("helloe");
-		return "hello user..Have a good day..";
-	}
-	
-	
-	
-	@RequestMapping(value = "/v10/user", method = RequestMethod.GET)
+    @RequestMapping(value = "/v10/user/{userId}/{passCode}", method = RequestMethod.GET)
     @ResponseBody
-    public String getAllUsers()  {
+    public String storeProfile(@PathVariable final String userId, @PathVariable final String passCode) {
+        try {
+            Statement stmt = openConnection();
+            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS User (userId varchar,passcode varchar,createDate timestamp,)");
+            String insertquery = "INSERT INTO User VALUES (" + userId + "," + passCode + ",now())";
+            System.out.println(insertquery);
+            stmt.executeUpdate(insertquery);
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return "User Inserted Successfully";
+    }
 
-	    Connection connection = null;
-	    Map<String, Object> attributes = new HashMap<>();
-	    try {
-	      connection = DatabaseUrl.extract().getConnection();
-         System.out.println(DatabaseUrl.extract().jdbcUrl());
-	      Statement stmt = connection.createStatement();
-	      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-	      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-	      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+    @RequestMapping(value = "/v10/firstUser", method = RequestMethod.GET)
+    @ResponseBody
+    public ArrayList<String> getAllUsers() {
 
-	      ArrayList<String> output = new ArrayList<String>();
-	      while (rs.next()) {
-	        output.add( "Read from DB: " + rs.getTimestamp("tick"));
-	        
-	      }
+        Connection connection = null;
+        try {
+            Statement stmt = openConnection();
+            ResultSet rs = stmt.executeQuery("SELECT userId FROM User");
+            ArrayList<String> output = new ArrayList<String>();
+            while (rs.next()) {
+                output.add("Read from DB: " + rs.getString("userId"));
+            }
+            return output;
+        } catch (Exception e) {
+            return null;
+        } finally {
+            if (connection != null)
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                }
+        }
+    }
 
-	      System.out.println("zill"+output.get(0));
-	      return "hello noted";
-	    } catch (Exception e) {
-	        System.out.println("zill null"+ e.getLocalizedMessage());
-	      return null;
-	    } finally {
-	      if (connection != null) try{connection.close();} catch(SQLException e){}
-	    }
-	  }
-
-
-	}
-	
-	
-	
-	
-
-
+}
