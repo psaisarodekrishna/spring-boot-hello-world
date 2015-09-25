5be121b2625f90df864ce80beede8f11793a44ea
psaisarodekrishna <psaisarodekrishna@deloitte.com>
2015-09-25 15:38:40 +0530

    Rollback

--- src/main/java/com/beta/mag/user/controller/UserController.java
+++ src/main/java/com/beta/mag/user/controller/UserController.java
@@ -13,12 +13,10 @@
 
 package com.beta.mag.user.controller;
 
-import java.net.URISyntaxException;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,60 +26,61 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.heroku.sdk.jdbc.DatabaseUrl;
 
+
 ;
 
 /**
- * The Class UserPersistence. controller is to create new user. returns the json response to the
- * REST client.
+ * The Class UserPersistence. controller is to create new user. returns the json
+ * response to the REST client.
  */
 @RestController
 @RequestMapping(value = "/api")
 public class UserController {
 
-  
 
-    @RequestMapping(value = "/v10/user/{userId}/{passCode}", method = RequestMethod.GET)
+
+	@RequestMapping(value = "/v10/user/{userId}/{passCode}", method = RequestMethod.GET)
+	@ResponseBody
+	public String getUserProfile(@PathVariable final String userId,
+			@PathVariable final String passCode)  {
+		System.out.println("helloe");
+		return "Hello User...Have a great Day";
+	}
+	
+	
+	
+	@RequestMapping(value = "/v10/currentTime", method = RequestMethod.GET)
     @ResponseBody
-    public String storeProfile(@PathVariable final String userId, @PathVariable final String passCode) throws URISyntaxException {
-        try {
-            Connection connection = null;
-            connection = DatabaseUrl.extract().getConnection();
-            Statement stmt = connection.createStatement();
-            
-            stmt.executeUpdate("CREATE TABLE IF NOT EXISTS user (userId varchar,passcode varchar,createDate timestamp)");
-            String insertquery = "INSERT INTO User VALUES (" + userId + "," + passCode + ",now())";
-            System.out.println(insertquery);
-            stmt.executeUpdate(insertquery);
-        } catch (SQLException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        return "User Inserted Successfully";
-    }
+    public String getAllUsers()  {
 
-    @RequestMapping(value = "/v10/firstUser", method = RequestMethod.GET)
-    @ResponseBody
-    public ArrayList<String> getAllUsers() {
+	    Connection connection = null;
+	    Map<String, Object> attributes = new HashMap<>();
+	    try {
+	      connection = DatabaseUrl.extract().getConnection();
+         System.out.println(DatabaseUrl.extract().jdbcUrl());
+	      Statement stmt = connection.createStatement();
+	      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+	      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+	      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
 
-        Connection connection = null;
-        try {
-            connection = DatabaseUrl.extract().getConnection();
-            Statement stmt = connection.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT userId FROM User");
-            ArrayList<String> output = new ArrayList<String>();
-            while (rs.next()) {
-                output.add("Read from DB: " + rs.getString("userId"));
-            }
-            return output;
-        } catch (Exception e) {
-            return null;
-        } finally {
-            if (connection != null)
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                }
-        }
-    }
+	      ArrayList<String> output = new ArrayList<String>();
+	      while (rs.next()) {
+	        output.add( "Read from DB: " + rs.getTimestamp("tick"));
+	      }
+	      return "Time now as per the Heroku Server Zone :"+output.get(0);
+	    } catch (Exception e) {
+	        System.out.println("zill null"+ e.getLocalizedMessage());
+	      return null;
+	    } finally {
+	      if (connection != null) try{connection.close();} catch(SQLException e){}
+	    }
+	  }
 
-}
+
+	}
+	
+	
+	
+	
+
+
