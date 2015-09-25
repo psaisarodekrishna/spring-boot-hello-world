a2ee87f45bf12cb384f32369bf305b27dbeba66d
psaisarodekrishna <psaisarodekrishna@deloitte.com>
2015-09-25 14:03:11 +0530

    yes done

--- src/main/java/com/beta/mag/user/controller/UserController.java
+++ src/main/java/com/beta/mag/user/controller/UserController.java
@@ -13,11 +13,18 @@
 
 package com.beta.mag.user.controller;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.heroku.sdk.jdbc.DatabaseUrl;
 
 
 ;
@@ -39,6 +46,36 @@ public class UserController {
 		System.out.println("helloe");
 		return null;
 	}
+	
+	
+	
+	@RequestMapping(value = "/v10/user", method = RequestMethod.GET)
+    @ResponseBody
+    public String getAllUsers()  {
+
+	    Connection connection = null;
+	    Map<String, Object> attributes = new HashMap<>();
+	    try {
+	      connection = DatabaseUrl.extract().getConnection();
+
+	      Statement stmt = connection.createStatement();
+	      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+	      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+	      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+
+	      ArrayList<String> output = new ArrayList<String>();
+	      while (rs.next()) {
+	        output.add( "Read from DB: " + rs.getTimestamp("tick"));
+	      }
+
+	      
+	      return "hello noted";
+	    } catch (Exception e) {
+	      return null;
+	    } finally {
+	      if (connection != null) try{connection.close();} catch(SQLException e){}
+	    }
+	  }
 
 
 	}
